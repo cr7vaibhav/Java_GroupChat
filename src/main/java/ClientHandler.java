38935658e1c939cb6784400f00499ab94088b058
cr7vaibhav <vaibhavkandwaloo7@gmail.com>
2024-07-30 12:38:01 +0530

    handling exceptions and dealing with blocking operation

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -4,27 +4,45 @@ import java.util.ArrayList;
 
 public class ClientHandler implements Runnable {
 
-    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();//keeps track of all our clients
-    private Socket socket;
-    private BufferedReader bufferedReader;//read and receive data
-    private BufferedWriter bufferedWriter;//send data
-    private String clientUsername;
+  public static ArrayList<ClientHandler> clientHandlers =
+      new ArrayList<>(); // keeps track of all our clients
+  private Socket socket;
+  private BufferedReader bufferedReader; // read and receive data
+  private BufferedWriter bufferedWriter; // send data
+  private String clientUsername;
 
-    public ClientHandler(Socket socket) {
-        try {
-            this.socket = socket;
-            this.bufferedWriter = new BufferedWriter(OutputStreamWriter(socket.getOutputStream()));//send
-            this.bufferedReader = new BufferedReader(InputStreamReader(socket.getInputStream()));//read
-            this.clientUsername = bufferedReader.readLine();
-            clientHandlers.add(this);
-            broadcastMessage("SERVER" + clientUsername + "has entered the chat!");
-        } catch (IOException e) {
-            closeEverything(socket, bufferedReader, bufferedWriter);
-        }
+  public ClientHandler(Socket socket) {
+    try {
+      this.socket = socket;
+      this.bufferedWriter =
+          new BufferedWriter(OutputStreamWriter(socket.getOutputStream())); // send
+      this.bufferedReader = new BufferedReader(InputStreamReader(socket.getInputStream())); // read
+      this.clientUsername = bufferedReader.readLine();
+      clientHandlers.add(this);
+      broadcastMessage("SERVER" + clientUsername + "has entered the chat!");
+    } catch (IOException e) {
+      closeEverything(socket, bufferedReader, bufferedWriter);
     }
+  }
 
-    @Override
-    public void run() {
+  @Override
+  public void
+      run() { // everything runs on a separate thread , what we want to do is listen for messages on
+              // a thread , since it's a blocking operation therefore we will have a separate thread
+              // waiting for messages and another for the rest of the application
+    String messageFromClient;
 
+    while (socket.isClosed()) {
+      try {
+        messageFromClient =
+            bufferedReader
+                .readLine(); // we want to run this on a separate thread so that the rest of the
+                             // program doesn't stop because of this line of code here
+        broadcastMessage(messageFromClient);
+      } catch (IOException e) {
+        closeEverything(socket,bufferedReader,bufferedWriter);
+        break; //without break this loop doesn't stop
+      }
     }
+  }
 }
