f66bdd24e6f6272972871eaf17ffb634e5dad4ea
cr7vaibhav <vaibhavkandwaloo7@gmail.com>
2024-07-30 12:59:05 +0530

    closing all the input output streams

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -15,8 +15,9 @@ public class ClientHandler implements Runnable {
     try {
       this.socket = socket;
       this.bufferedWriter =
-          new BufferedWriter(OutputStreamWriter(socket.getOutputStream())); // send
-      this.bufferedReader = new BufferedReader(InputStreamReader(socket.getInputStream())); // read
+          new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())); // send
+      this.bufferedReader =
+          new BufferedReader(new InputStreamReader(socket.getInputStream())); // read
       this.clientUsername = bufferedReader.readLine();
       clientHandlers.add(this);
       broadcastMessage("SERVER" + clientUsername + "has entered the chat!");
@@ -28,8 +29,8 @@ public class ClientHandler implements Runnable {
   @Override
   public void
       run() { // everything runs on a separate thread , what we want to do is listen for messages on
-              // a thread , since it's a blocking operation therefore we will have a separate thread
-              // waiting for messages and another for the rest of the application
+    // a thread , since it's a blocking operation therefore we will have a separate thread
+    // waiting for messages and another for the rest of the application
     String messageFromClient;
 
     while (socket.isClosed()) {
@@ -37,12 +38,52 @@ public class ClientHandler implements Runnable {
         messageFromClient =
             bufferedReader
                 .readLine(); // we want to run this on a separate thread so that the rest of the
-                             // program doesn't stop because of this line of code here
+        // program doesn't stop because of this line of code here
         broadcastMessage(messageFromClient);
       } catch (IOException e) {
-        closeEverything(socket,bufferedReader,bufferedWriter);
-        break; //without break this loop doesn't stop
+        closeEverything(socket, bufferedReader, bufferedWriter);
+        break; // without break this loop doesn't stop
       }
+    }
+  }
+
+  public void broadcastMessage(String messasgetoSend) {
+    for (ClientHandler clientHandler : clientHandlers) {
+      try {
+        if (!clientHandler.clientUsername.equals(clientUsername)) {
+          clientHandler.bufferedWriter.write(messasgetoSend);
+          clientHandler.bufferedWriter.newLine();
+          clientHandler.bufferedWriter.flush();
+        }
+
+      } catch (IOException e) {
+        closeEverything(socket, bufferedReader, bufferedWriter);
+      }
+    }
+  }
+
+  public void removeClientHandler() {
+    clientHandlers.remove(this);
+    broadcastMessage("SERVER: " + clientUsername + " has left the chat!");
+  }
+
+  public void closeEverything(
+      Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+    removeClientHandler();
+    try {
+      if (bufferedReader != null) {
+        bufferedReader.close();
+      }
+
+      if (bufferedWriter != null) {
+        bufferedWriter.close();
+      }
+
+      if (socket != null) {
+        socket.close();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 }
